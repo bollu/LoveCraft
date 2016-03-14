abb06a59741ba67ae444459dcc62a1374dc6f43a
bollu <siddu.druid@gmail.com>
2016-03-14 10:34:37 +0530

    added SDL as a library

--- lovecraft/src/main.c
+++ lovecraft/src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-
+#include <sdl/SDL.h>
 
 int main() {
     printf("hello, world");
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
+        fprintf(stderr,
+                "\nUnable to initialize SDL:  %s\n",
+                SDL_GetError()
+               );
+        return 1;
+    }
+
     return 1;
 }
