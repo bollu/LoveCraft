751049efbb08badf833676b285a6bcd641d741b6
bollu <siddu.druid@gmail.com>
2016-03-17 00:30:49 +0530

    added proper openGL context

--- lovecraft/src/main.c
+++ lovecraft/src/main.c
@@ -4,40 +4,43 @@
 #include <stdio.h>
 #include <sdl/SDL.h>
 #include <sdl/SDL_opengl.h>
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
     SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+
 
     SDL_GLContext context = SDL_GL_CreateContext(window);
     
     glewExperimental = GL_TRUE;
     glewInit();
     
-    GLuint vertexBuffer;
-    glGenBuffers(1, &vertexBuffer);
-
-    printf("%u\n", vertexBuffer);
-
     SDL_Event windowEvent;
-    while (1)
-    {
-        if (SDL_PollEvent(&windowEvent))
-        {
+
+    while (1) {   
+
+        while (SDL_PollEvent(&windowEvent)) {
             if (windowEvent.type == SDL_QUIT) break;
+            if(windowEvent.type == SDL_KEYDOWN) {
+                if (windowEvent.key.keysym.sym == SDLK_q) { break; }
+            }
         }
 
+        glClearColor(128, 255, 0, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
         SDL_GL_SwapWindow(window);
         SDL_Delay((int)(1.0 / 60.0 * 1000));
     }
 
     SDL_GL_DeleteContext(context);
-
-
+    SDL_DestroyWindow(window);
     SDL_Quit();
+    
     return 0;
 }
