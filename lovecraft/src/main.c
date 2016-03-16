cc8aca2846357fa2081a773443b980bd95d9b817
bollu <siddu.druid@gmail.com>
2016-03-17 01:07:17 +0530

    got a simple event management system up

--- lovecraft/src/main.c
+++ lovecraft/src/main.c
@@ -1,9 +1,48 @@
 #define GLEW_STATIC
 #include <glew/glew.h>
-
 #include <stdio.h>
 #include <sdl/SDL.h>
 #include <sdl/SDL_opengl.h>
+
+typedef enum {
+    NoEvent,
+    QuitEvent,
+} GameEventType;
+
+typedef union  {
+    GameEventType type;
+} GameEvent;
+
+
+
+typedef struct {
+    SDL_Keycode key_escape;
+    SDL_Keycode key_left;
+} Settings;
+
+
+GameEvent game_event_from_raw(const SDL_Event *event, const Settings *settings) {
+    GameEvent game_event;
+
+    if (event->type == SDL_QUIT) {
+        game_event.type = QuitEvent;
+    }
+    else if (event->type == SDL_KEYDOWN) {
+        if (event->key.keysym.sym == settings->key_escape){
+            game_event.type = QuitEvent;
+        }
+    }
+    return game_event;
+};
+
+Settings load_settings(const char* filepath) {
+    Settings s;
+    s.key_escape = SDLK_ESCAPE;
+    s.key_left = SDLK_a;
+    return s;
+}
+
+
 int main(int argc, char *argv[]) {
 
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -14,28 +53,25 @@ int main(int argc, char *argv[]) {
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
     SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
-
-
     SDL_GLContext context = SDL_GL_CreateContext(window);
     
     glewExperimental = GL_TRUE;
     glewInit();
+    glClearColor(0.1, 0.2, 0.3, 0);
     
-    SDL_Event windowEvent;
+    SDL_Event event;
+    int should_quit = 0;
 
-    while (1) {   
-
-        while (SDL_PollEvent(&windowEvent)) {
-            if (windowEvent.type == SDL_QUIT) break;
-            if(windowEvent.type == SDL_KEYDOWN) {
-                if (windowEvent.key.keysym.sym == SDLK_q) { break; }
-            }
+    Settings settings = load_settings("settings.cfg");
+    
+    while (!should_quit) {
+        while (SDL_PollEvent(&event)) {
+            GameEvent ge = game_event_from_raw(&event, &settings);
+            should_quit = ge.type == QuitEvent;
         }
 
-        glClearColor(128, 255, 0, 0);
         glClear(GL_COLOR_BUFFER_BIT);
         SDL_GL_SwapWindow(window);
-        SDL_Delay((int)(1.0 / 60.0 * 1000));
     }
 
     SDL_GL_DeleteContext(context);
